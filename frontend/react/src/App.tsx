8b4b71a9b80bbdf7b10c17680f2f85c4c1937278
Fernando Gazzana <fernando.gazzana@gmail.com>
2025-04-20 03:48:46 -0300

    Add BattleCard, Edit Header, Add BattlePage

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,14 +1,15 @@
 //import { useState } from 'react'
 import './App.css'
-import {SignUpPage} from "./pages/SignUpPage.tsx";
-import {Header} from "./components/Header.tsx";
+//import {SignUpPage} from "./pages/SignUpPage.tsx";
+import {BattlePage} from "./pages/BattlePage.tsx"
+
 function App() {
 //  const [count, setCount] = useState(0)
 
     return (
         <>
-           <Header/>
-           <SignUpPage/>
+            {/*<SignUpPage/>*/}
+            <BattlePage/>
         </>
     )
 }
---
+++ frontend/react/src/interfaces/Duel.ts
@@ -0,0 +1,6 @@
+import {Team} from "./Team.ts";
+
+export interface Duel {
+    team1: Team,
+    team2: Team,
+}
